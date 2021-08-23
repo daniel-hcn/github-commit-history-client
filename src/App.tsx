e7b49c6f222beaa0033944a84206a589ec9ed4db
Daniel <daniel.cn94@gmail.com>
2021-08-22 20:31:36 -0500

    Implement functionality to the interface
    - Add input for selection of the repository to consult
    - Add input for the branch to consult

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,46 @@
-import React, { useEffect, useState } from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [state, setState] = useState({
+    repository: 'daniel-hcn/github-commit-history-server',
+    branch: 'master',
+  });
   const [branches, setBranches] = useState([]);
   const [commits, setCommits] = useState([]);
+
+  const getBranches = async (): Promise<void> => {
+    const response = await axios.get(`/github/repository/${encodeURIComponent(state.repository)}/branches`);
+    setBranches(response.data);
+  };
+
+  const getCommits = async (): Promise<void> => {
+    const response = await axios.get(`/github/repository/${encodeURIComponent(state.repository)}/commits?branchName=${state.branch}`);
+    setCommits(response.data);
+  };
+
+  const handleChange = (attrName: string, selectedOption: string) => {
+    setState({ ...state, [attrName]: selectedOption });
+  };
 
   return (
     <div className="App">
       <h1>Github Commit History</h1>
-      <select>
+
+      <select onChange={(e) => handleChange('repository', e.currentTarget.value)}>
+        <option value='daniel-hcn/github-commit-history-server'>daniel-hcn/github-commit-history-server</option>
+        <option value='daniel-hcn/github-commit-history-client'>daniel-hcn/github-commit-history-client</option>
+      </select>
+      <button onClick={() => getBranches()}>Get Branches</button>
+
+      <select onChange={(e) => handleChange('branch', e.currentTarget.value)}>
         {branches.map(({ name }, i) => (
           <option key={i} value={name}>{name}</option>
         ))}
       </select>
+      <button onClick={() => getCommits()}>Get Commits</button>
+
       {commits.map((commit: any, i) => (
         <li key={i}> {commit.message} </li>
       ))}
