5cf067dc3a598e604c6483137bbb0abceab88eab
Daniel <daniel.cn94@gmail.com>
2021-08-22 19:43:48 -0500

    Implement initial design
    - Add axios to package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [branches, setBranches] = useState([]);
+  const [commits, setCommits] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Github Commit History</h1>
+      <select>
+        {branches.map(({ name }, i) => (
+          <option key={i} value={name}>{name}</option>
+        ))}
+      </select>
+      {commits.map((commit: any, i) => (
+        <li key={i}> {commit.message} </li>
+      ))}
     </div>
   );
 }
